b48f3be1790b7b4a3d27b7e36ed1c29e86061257
Thawan Marques <thawan28032001@gmail.com>
2022-12-02 22:17:28 -0300

    Fourth project commit

--- login/LoginController.js
+++ login/LoginController.js
@@ -6,4 +6,15 @@ router.get('/login', (req, res) => {
     res.render('index')
 });
 
+router.get('/create', (req, res) => {
+    res.render('create')
+});
+
+router.post('/create', (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    res.json({email, password});
+});
+
 module.exports = router;
