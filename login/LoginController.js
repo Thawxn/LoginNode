d6d81535ca714afb4216571aeeadf8578673c446
Thawan Marques <thawan28032001@gmail.com>
2022-12-03 16:09:32 -0300

    Third project commit

--- login/LoginController.js
+++ login/LoginController.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Login = require('./Login');
+const bcrypt =require('bcryptjs')
+
+router.get('/', (req, res) => {
+    res.render('home');
+})
 
 router.get('/login', (req, res) => {
     res.render('index')
@@ -14,7 +19,23 @@ router.post('/create', (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    res.json({email, password});
+    Login.findOne({where: { email: email}}).then(login => {
+        if(login == undefined){
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+
+            Login.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect('/')
+            }).catch((erro) => {
+                res.redirect('/')
+            });
+        }else{
+            res.send('Email já criado');
+        };
+    })
 });
 
 module.exports = router;
